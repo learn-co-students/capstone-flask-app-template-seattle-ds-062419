a4a36f4862a968e1e92e4a2bc70c5e1d2c1c5dc1
hoffm386 <erinrhof@uw.edu>
2019-09-23 15:37:44 -0700

    setup for second lecture

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from time import strftime
 
 app = Flask(__name__, static_url_path="/static")
@@ -10,3 +10,10 @@ def index():
     print(time_str)
     return render_template("index.html", time_info=time_str)
 
+@app.route("/get_results", methods=["POST"])
+def get_results():
+    data = request.form
+    print(data)
+    user_number = int(data["number"])
+    user_number_doubled = user_number * 2
+    return render_template("results.html", output_num=user_number_doubled)
